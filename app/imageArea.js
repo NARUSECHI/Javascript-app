e0a18bfaff36457ddbe0746424e5748508d3bdda
Naruse Chiho <penyo6697@gmail.com>
2023-01-22 19:59:19 +0900

    Add Index

---
+++ app/imageArea.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', e => {
+    const elImgA = document.querySelector('#imageArea');
+    const elNoView = document.querySelector('noView');
+    const elFile = document.querySelector('#file');
+
+    const canvas = document.querySelector('#view');
+    const context = canvas.getContext('2d');
+
+    elImgA.addEventListener
+})

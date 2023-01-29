4669405fdd3e04fbb88cf9dde8585682bb16ca32
Naruse Chiho <penyo6697@gmail.com>
2023-01-29 22:05:28 +0900

    Add img area file space

--- app/imageArea.js
+++ app/imageArea.js
@@ -6,5 +6,34 @@ window.addEventListener('DOMContentLoaded', e => {
     const canvas = document.querySelector('#view');
     const context = canvas.getContext('2d');
 
-    elImgA.addEventListener
+    elImgA.addEventListener('dragover',e => {
+        e.preventDefault();
+    });
+    elImgA.addEventListener('dragleave', e => {
+        e.preventDefault();
+    });
+    // move file from user to browser
+    elImgA.addEventListener('drop', e => {
+        e.preventDefault();
+        uiReadImage(e.dataTransfer.files[0]);
+    });
+
+    elImgA.addEventListener('click', e => {
+        elFile.click();
+    });
+    elFile.addEventListener('change', e => {
+        uiReadImage(e.target.files[0]);
+    });
+
+    //function
+
+    async function uiReadImage(file){
+        try {
+            const dtURL = await uiReadImage(file);
+            await drawImage(dtURL, canvas, context);
+        }catch(e) {
+            return;
+        }
+    }
+
 })
